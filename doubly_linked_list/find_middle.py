68a1fede0cfe8d197e4ae274daea2e9799445635
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2020-03-12 20:29:37 -0400

    work along

---
+++ doubly_linked_list/find_middle.py
@@ -0,0 +1,8 @@
+from doubly_linked_list import DoublyLinkedList
+
+# return the middle node of the DLL, if there are two nodes, return the left one
+# no empty list, length >= 1
+# 1 - 2 - 3 : 2
+# 1 - 2 - 3 - 4: 2
+def find_middle(dll):
+    pass
--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -7,13 +7,15 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage =  DoublyLinkedList()
 
+    # Insert
     def enqueue(self, value):
-        pass
+        self.storage.add_to_tail(value)
 
+    # Delete
     def dequeue(self):
-        pass
+        return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        return self.storage.length
